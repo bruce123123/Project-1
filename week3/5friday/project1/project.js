ad3e1733efd7284df32ff7a91fbee907645ea94a
bruce-h <bdhay@protonmail.com>
2021-01-10 16:41:03 -0600

    color arrays, initial

--- week3/5friday/project1/project.js
+++ week3/5friday/project1/project.js
@@ -1,2 +1,11 @@
 console.log('project is working');
 
+const colors=['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'lightpurple', 'salmon'];
+const cholor2=['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'lightpurple', 'salmon'];
+
+function ClearArrays() {
+    
+    colorPick.length = 0;
+    pickedColors.length = 0;
+    random.length = 0;
+  }
