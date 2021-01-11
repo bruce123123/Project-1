0141a288c64395c23835b1891e43da609a0cf47a
bruce-h <bdhay@protonmail.com>
2021-01-11 15:32:23 -0600

    js add consts

--- week3/5friday/project1/project.js
+++ week3/5friday/project1/project.js
@@ -2,32 +2,36 @@ const colors=['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'lightpurple
 const cholor2=['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'lightpurple', 'salmon'];
 const random=['red','blue','plum', 'salmon', 'blue', 'yellow','purple','green', 'green','orange','red','yellow', 'orange', 'purple', 'plum', 'salmon'];
 
-let turn = 0;
-const red = [];
-const blue = [];
-const wrapper = document.querySelector('#wrapper');
-const one = document.querySelector('#one');
-const two = document.querySelector('#two');
-const three = document.querySelector('#three');
-const four = document.querySelector('#four');
-const five = document.querySelector('#five');
-const six = document.querySelector('#six');
-const seven = document.querySelector('#seven');
-const eight = document.querySelector('#eight');
-const nine = document.querySelector('#nine');
-const ten = document.querySelector('#ten');
-const eleven = document.querySelector('#eleven');
-const twelve = document.querySelector('#twelve');
-const thirteen = document.querySelector('#thirteen');
-const fourteen = document.querySelector('#fourteen');
-const fifteen = document.querySelector('#fifteen');
-const sixteen = document.querySelector('#sixteen');
-
-
-function startGame() {
-  pick1.length = 0;
-  pick2.length = 0;
-  };
+  let turn = 0;
+  const red = [];
+  const blue = [];
+  const wrapper = document.querySelector('#wrapper');
+  const one = document.querySelector('#one');
+  const two = document.querySelector('#two');
+  const three = document.querySelector('#three');
+  const four = document.querySelector('#four');
+  const five = document.querySelector('#five');
+  const six = document.querySelector('#six');
+  const seven = document.querySelector('#seven');
+  const eight = document.querySelector('#eight');
+  const nine = document.querySelector('#nine');
+  const ten = document.querySelector('#ten');
+  const eleven = document.querySelector('#eleven');
+  const twelve = document.querySelector('#twelve');
+  const thirteen = document.querySelector('#thirteen');
+  const fourteen = document.querySelector('#fourteen');
+  const fifteen = document.querySelector('#fifteen');
+  const sixteen = document.querySelector('#sixteen');
+  const clearBoard = document.querySelector('clearBoard');
+  
+  
+  clearBoard.addEventListener('click', startGame)
+    
+  function startGame() {
+      pick1.length = 0;
+      pick2.length = 0;
+      console.log('new game');
+      }
 
 
 // document.querySelector('#reset').addEventListener('click', startGame);
@@ -59,7 +63,44 @@ function ClearArrays() {
   } else {
       pick1.pop;
       pick2.pop;
+      
       //change square background back to white.
   }
 }
+  });
+  two.addEventListener("click", function() {
+    if (pick1.length !== pick2.length) {
+        one.classList.add('blue');
+        pick1.push['blue']
+        console.log(turn);
+    } 
+     else if (pick1.length == pick2.length) {
+    one.classList.add('blue');
+    pick2.push['blue'];
+    if (JSON.stringify(pick1) === JSON.stringify(pick2)) {
+      return;
+  } else {
+      pick1.pop;
+      pick2.pop;
+      //change square background back to white.
+  }
+}
+  });
+  three.addEventListener("click", function() {
+    if (pick1.length !== pick2.length) {
+        one.classList.add('plum');
+        pick1.push['plum']
+        console.log(turn);
+    } 
+     else if (pick1.length == pick2.length) {
+    one.classList.add('plum');
+    pick2.push['plum'];
+    if (JSON.stringify(pick1) === JSON.stringify(pick2)) {
+      return;
+  } else {
+      pick1.pop;
+      pick2.pop;
+      //change square background back to white.
+  }
+  }
   });
